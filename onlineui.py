b0c15ade8f62b91843d513e88434d6409aae6f9b
Andrew Gu <andrewgux3@gmail.com>
2016-10-12 11:29:22 -0700

    networked implementation stable

---
+++ onlineui.py
@@ -0,0 +1,15 @@
+import socket
+import connectfour
+import console
+import userfunctions
+
+def start_client():#Run after connecting to server & receiving 'READY'
+    game = userfunctions.start_game()
+
+def user_move():
+
+
+def main():
+
+if __name__ == '__main__';
+    main()
